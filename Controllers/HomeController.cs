8e31d5e7295226f154ce44b7e710b79b1644a357
msxxalpha <alirezaaminfallah@gmail.com>
2026-09-22 15:46:37 +0330

    fix: add exception handler endpoint

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Indamin.Payment.Data;using Microsoft.AspNetCore.Authorization;using Microsoft.AspNetCore.Mvc;using Microsoft.EntityFrameworkCore;
 namespace Indamin.Payment.Controllers;
 [Authorize]public class HomeController(AppDbContext db):Controller{
+ public IActionResult Error()=>View();
  public async Task<IActionResult> Index()=>View(new HomeVm{
   Name=User.Identity?.Name??"کاربر",Parts=await db.Parts.CountAsync(x=>x.IsActive),Suppliers=await db.Suppliers.CountAsync(x=>x.IsActive),
   Mappings=await db.SupplierParts.CountAsync(x=>x.IsActive),Parameters=await db.PaymentParameters.CountAsync(x=>x.IsActive),
