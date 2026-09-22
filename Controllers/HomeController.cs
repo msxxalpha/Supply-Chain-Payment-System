952aab9a502450506ccc96552351faebe874d00a
msxxalpha <alirezaaminfallah@gmail.com>
2026-09-22 15:51:52 +0330

    fix: keep HomeVm nested in HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,5 +7,5 @@ namespace Indamin.Payment.Controllers;
   Mappings=await db.SupplierParts.CountAsync(x=>x.IsActive),Parameters=await db.PaymentParameters.CountAsync(x=>x.IsActive),
   ApprovedRuns=await db.PaymentRuns.CountAsync(x=>x.Status==PaymentRunStatus.Approved),
   TotalAllocated=await db.PaymentRunInvoices.Where(x=>x.PaymentRun!.Status==PaymentRunStatus.Approved).SumAsync(x=>(decimal?)x.AllocatedAmount)??0
- });}
+ });
  public record HomeVm{public string Name{get;init;}="";public int Parts{get;init;}public int Suppliers{get;init;}public int Mappings{get;init;}public int Parameters{get;init;}public int ApprovedRuns{get;init;}public decimal TotalAllocated{get;init;}}
